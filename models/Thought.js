089320333ac0dc0cd0cfee8af5efe77d87fd2d60
Courtney Jensen <courtneyajensen14@gmail.com>
2023-01-17 16:35:05 -0700

    models

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schemad(
+    {
+        thoughtText: {
+            type: String,
+            required: "Add a thought!",
+            minlength: 1,
+            maxlength: 300,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+thoughtSchema. virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
